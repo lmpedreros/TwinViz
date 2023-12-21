6aab077342c7f1d9a161c6b00d955b4ed86d4ed7
lucas pedreros <lmpedreros@uc.cl>
2023-12-21 10:31:05 -0300

    BorradoDeCosas

--- 01_Camara_Mina/predict2.py
+++
@@ -1,9 +0,0 @@
-from ultralytics import YOLO
-import os
-
-# Load a model
-weights = os.path.join('.', 'runs', 'detect', 'train13', 'weights', 'best.pt')
-model = YOLO(weights)  # load a custom model
-
-# Predict with the model
-results = model('./Data/Images/images/00c133189dd16a45.jpg')  # predict on an image
